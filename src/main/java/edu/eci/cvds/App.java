5ac2e960da766d2d8727ae970214c0a5867b99f2
Mohcer <felipebarrera104@gmail.com>
2024-08-28 21:33:17 -0500

    Pruebas de ShapeMain completadas

---
+++ src/main/java/edu/eci/cvds/App.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hola mi nombre es " +  args[0] + " y soy un guia explorador de la tropa 53 guarida 12");
+    }
+}
